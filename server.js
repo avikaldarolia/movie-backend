ed4b175889b5230b941c9e64f86ae795631af480
Avikal Darolia <avikaldarolia7@gmail.com>
2022-12-22 00:20:35 +0530

    Chat feature working

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const http = require('http')
 const utils = require('./utils/utils')
 // const Sequelize = require('./sequelize');
 const path = require('path');
+const { Server } = require('socket.io')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.disable('x-powered-by');
+
+// const server = http.createServer(app)
+
+
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -27,8 +33,34 @@ require(__dirname + '/routes/').forEach(function (route) {
 app.use(utils.errorHandler);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+let server = app.listen(PORT)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data)
+    console.log(`User with ID:${socket.id} joined room ${data}`);
+  })
+
+  socket.on('send_message', (data) => {
+    console.log(data);
+    socket.to(data.room).emit('receive_message', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected', socket.id);
+  })
+})
+console.log(`Example app listening at http://localhost:${PORT}`);
+// let server = app.listen(PORT, () => {
+//   console.log(`Example app listening at http://localhost:${PORT}`);
+// });
 
 
